2aa488d2fc532bb1aad6551ab95e7f78ab30bec9
KoolP <patrik.rikama@gmail.com>
2018-04-09 12:08:33 +0200

    Tower of Doom api serach for temp

--- GalacticWeather/ViewController.swift
+++ GalacticWeather/ViewController.swift
@@ -8,18 +8,209 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var spacePicBarButton: UIBarButtonItem!
+    @IBOutlet weak var searchTitle: UILabel!
+    @IBOutlet weak var temp: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var condition: UILabel!
+    @IBOutlet weak var bottomBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navbarStyle()
+        searchBar.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        spacePicBarButton.isEnabled = false
+        spacePicBarButton.isEnabled = true
+    }
+    
+    //API search for temp, tower of doom
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Search button/enter pressed")
+        
+        //Double if let in begginning, %20% for space in search
+        
+        if let safeString = searchBar.text!.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed),
+            let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(safeString)&format=json&pretty=1&appid=a46f63c4899f9a64dc67f7114b104f2b") {
+            
+            let request = URLRequest(url: url)
+            let task = URLSession.shared.dataTask(with: request, completionHandler:
+            { (data : Data?, response : URLResponse?, error: Error?) in
+                if let actualError = error {
+                    print(actualError)
+                } else {
+                    if let actualData = data {
+                        
+                        let options = JSONSerialization.ReadingOptions()
+                        do {
+                            let parsed = try JSONSerialization.jsonObject(with: actualData, options: options)
+                            //print(parsed)
+                            
+                            //parsing form server answer, error handling first
+                            if let dict = parsed as? [String: AnyObject] {
+                                if let mainWeather = dict["main"] {
+                                    
+                                    
+                                    if let tempData = mainWeather["temp"] {
+                                        if let actualTemp = tempData as? Double {
+                                            //print(actualData)
+                                            //print(tempData!)
+                                            DispatchQueue.main.async {
+                                                self.temp.text = String(format: "%.1f° C", actualTemp - 273.15)
+                                            }
+                                            //self.temp.text = String(format: "%.1f°", tempData)
+                                        } else {
+                                            print("Temp was not a Double")
+                                        }
+                                    } else {
+                                        print("First topic was not a double")
+                                    }
+                                    
+                                    
+                                    //print(mainWeather)
+                                } else {
+                                    print("Cant find main key value")
+                                }
+                            } else {
+                                print("Can't create dict.")
+                            }
+                            
+                        } catch {
+                            print("Failed to parse json")
+                        }
+                        
+                    } else {
+                        print("Data was nil")
+                    }
+                }
+            })
+            
+            task.resume()
+            
+        } else {
+            print("Bad URL")
+        }
+        
+        
+    }
+    
+    func navbarStyle() {
+        //NavBar
+        let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.3)
+        
+        spacePicBarButton.setTitleTextAttributes([ NSAttributedStringKey.font: UIFont(name: "Futura", size: 14)!], for: UIControlState.normal)
+        spacePicBarButton.title = "🚀"
+        searchTitle.text = "🌍"
+        
+        searchTitle.font = UIFont(name:"American Typewriter", size: 14)
+        temp.font = UIFont(name:"American Typewriter", size: 14)
+        city.font = UIFont(name:"American Typewriter", size: 14)
+        condition.font = UIFont(name:"American Typewriter", size: 10)
+        bottomBackground.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.3)
+        bottomBackground.layer.cornerRadius = 12
+        
+        //SearchBar
+        let searchTextField: UITextField? = searchBar.value(forKey: "searchField") as? UITextField
+        if searchTextField!.responds(to: #selector(getter: UITextField.attributedPlaceholder)) {
+            let attributeDict = [NSAttributedStringKey.foregroundColor: UIColor.lightGray]
+            searchTextField!.attributedPlaceholder = NSAttributedString(string: "Search city", attributes: attributeDict)
+        }
+        let glassIconView = searchTextField?.leftView as? UIImageView
+        
+        glassIconView?.image = glassIconView?.image?.withRenderingMode(.alwaysTemplate)
+        glassIconView?.tintColor = UIColor.lightGray
+    }
 
 
 }
+
+
+
+
+
+
+
+////API search for temp in Celsius via searchBar and from Open WeatherAPI
+////This is the tower of doom code without decodable from Swift 4
+//func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//    print("Search button/enter pressed")
+//
+//    //Double if let in begginning, %20% for space in search
+//
+//    if let safeString = searchBar.text!.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed),
+//        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(safeString)&format=json&pretty=1&appid=a46f63c4899f9a64dc67f7114b104f2b") {
+//
+//        let request = URLRequest(url: url)
+//        let task = URLSession.shared.dataTask(with: request, completionHandler:
+//        { (data : Data?, response : URLResponse?, error: Error?) in
+//            if let actualError = error {
+//                print(actualError)
+//            } else {
+//                if let actualData = data {
+//
+//                    let options = JSONSerialization.ReadingOptions()
+//                    do {
+//                        let parsed = try JSONSerialization.jsonObject(with: actualData, options: options)
+//                        //print(parsed)
+//
+//                        //parsing form server answer, error handling first
+//                        if let dict = parsed as? [String: AnyObject] {
+//                            if let mainWeather = dict["main"] {
+//
+//
+//                                if let tempData = mainWeather["temp"] {
+//                                    if let actualTemp = tempData as? Double {
+//                                        //print(actualData)
+//                                        //print(tempData!)
+//                                        DispatchQueue.main.async {
+//                                            self.temp.text = String(format: "%.1f° C", actualTemp - 273.15)
+//                                        }
+//                                        //self.temp.text = String(format: "%.1f°", tempData)
+//                                    } else {
+//                                        print("Temp was not a Double")
+//                                    }
+//                                } else {
+//                                    print("First topic was not a double")
+//                                }
+//
+//
+//                                //print(mainWeather)
+//                            } else {
+//                                print("Cant find main key value")
+//                            }
+//                        } else {
+//                            print("Can't create dict.")
+//                        }
+//
+//                    } catch {
+//                        print("Failed to parse json")
+//                    }
+//
+//                } else {
+//                    print("Data was nil")
+//                }
+//            }
+//        })
+//
+//        task.resume()
+//
+//    } else {
+//        print("Bad URL")
+//    }
+//
+//
+//}
 
